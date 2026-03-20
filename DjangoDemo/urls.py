1395afc4a192fcf5fcb11a1e02a95375194b9dbb
Eric <eric@example.com>
2026-03-20 10:59:46 +0800

    Move health check endpoint to root route

--- DjangoDemo/urls.py
+++ DjangoDemo/urls.py
@@ -2,8 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from demo.views import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('demo.urls')),
+    path('health_check/', health_check, name='health_check'),
 ]
--- demo/urls.py
+++ demo/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('error-log/', views.error_log, name='error_log'),
     path('http-error-500/', views.http_error_500, name='http_error_500'),
     path('http-error-404/', views.http_error_404, name='http_error_404'),
-    path('health_check/', views.health_check, name='health_check'),
 ]
